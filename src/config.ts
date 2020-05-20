7cd0b708b313280d81d85ade47c70ceb977f5db3
mhchia <kevin.mh.chia@gmail.com>
2020-05-20 17:28:07 +0800

    Change ip to domain name

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 type ServerConfig = { host: string, port: number, path: string };
 
 const defaultServerConfig: ServerConfig = {
-  host: '18.180.165.244',  // A temporary peerjs-server
+  host: 'peekabookpeerserver.mhchia.com',  // A temporary peerjs-server
   port: 8000,
   path: '/myapp',
 };
