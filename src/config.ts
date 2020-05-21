9c80b911bc6704bc7c7125fdc61bb5f7e0f750c5
mhchia <kevin.mh.chia@gmail.com>
2020-05-21 23:29:59 +0800

    Fix: always use `secure: true` in client
    This makes the client work both with ws and wss

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
-type ServerConfig = { host: string, port: number, path: string, debug?: number};
+type ServerConfig = { host: string, port: number, path: string, secure: boolean, debug?: number};
 
 const defaultServerConfig: ServerConfig = {
   host: 'peekabookpeerserver.mhchia.com',  // A temporary peerjs-server
   port: 8000,
   path: '/myapp',
+  secure: true,
   debug: 3,  // Log more for debugging
 };
 
