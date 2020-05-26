5f76cc3c490bbdc8779cdb39174e947ed9766afb
mhchia <kevin.mh.chia@gmail.com>
2020-05-26 23:17:20 +0800

    Add TODO in test

---
+++ test/smpPeer.test.ts
@@ -0,0 +1,15 @@
+// import SMPPeer from '../src/smpPeer';
+
+describe('', () => {
+  test('test', async () => {
+    // TODO: peerjs doesn't work w/ nodejs. Figure out a way to run tests.
+    //  Ref:
+    //    - https://github.com/mhchia/peek-a-book/issues/2
+    //    - https://github.com/peers/peerjs/issues/606
+    //  Error: "node_modules/peerjs/dist/peerjs.min.js:48 ERROR PeerJS:  Error: The current browser does not support WebRTC"
+
+    // const pid = '123456';
+    // const t = new SMPPeer('1');
+    // await t.connectToPeerServer(pid);
+  });
+});
