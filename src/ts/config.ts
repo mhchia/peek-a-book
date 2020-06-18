602cc1d5352b035eeb843fc3256beaab271ffe58
mhchia <kevin.mh.chia@gmail.com>
2020-06-18 17:21:59 +0800

    Separate configs by network name

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,2 +1,11 @@
-export const contractAtBlock = 8090511;
-export const contractAddress = '0x74EF33Eb7aa3fF607E18aa240dD7176574c62d23';
+export const networkConfig: {
+  [network: string]: {
+    contractAtBlock: number;
+    contractAddress: string;
+  };
+} = {
+  ropsten: {
+    contractAtBlock: 8090511,
+    contractAddress: '0x74EF33Eb7aa3fF607E18aa240dD7176574c62d23',
+  },
+};
