1ac803bf306f20f1722f77b0691871b036870d7f
mhchia <mhchia@users.noreply.github.com>
2020-06-23 09:59:28 +0000

    Update gh-pages to output generated at 3e5c67c

--- js/config.d.ts
+++ js/config.d.ts
@@ -1,2 +1,7 @@
-export declare const contractAtBlock = 8090511;
-export declare const contractAddress = "0x74EF33Eb7aa3fF607E18aa240dD7176574c62d23";
+export declare const networkConfig: {
+    [network: string]: {
+        contractAtBlock: number;
+        contractAddress: string;
+    };
+};
+export declare const peerServerGetPeersURL: string;
