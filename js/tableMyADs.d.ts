d8f83686415d14b5b071685ec294cd389dd91dce
mhchia <mhchia@users.noreply.github.com>
2020-07-02 09:26:19 +0000

    Update gh-pages to output generated at 65b2c1a

--- js/tableMyADs.d.ts
+++ js/tableMyADs.d.ts
@@ -7,10 +7,7 @@ import { BigNumber } from 'ethers/utils';
 export declare const buttonNewAD: HTMLButtonElement;
 export declare function cbButtonNewAD(contract: PeekABookContract, config: TNetworkConfig): Promise<void>;
 export declare function updateMyADsTable(contract: PeekABookContract, myAddr: string): Promise<void>;
-/**
- * Formatter and events
- */
-export declare function tableMyADsListenFormatter(adID: BigNumber, currency1: string, currency2: string): string;
+export declare function tableMyADsListenFormatter(adID: BigNumber, peerID: string, currency1: string, currency2: string): string;
 export declare function tableMyADsListenButtonCB(adID: BigNumber, peerID: string): Promise<void>;
 export declare function tableMyADsDeleteFormatter(): string;
 export declare function tableMyADsDeleteCB(contract: PeekABookContract, config: TNetworkConfig, adID: BigNumber): Promise<void>;
